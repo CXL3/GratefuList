fc1b803469b1d187fa7c9f67c2bc74db0d9f4d06
Claire Lu <CXL3@users.noreply.github.com>
2017-11-02 19:31:38 -0700

    update 1
    Add Layout and Variables

--- Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/MainActivity.java
+++ Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/MainActivity.java
@@ -2,12 +2,24 @@ package com.appfunlu.lawofattraction;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputGratefulList;
+
+    private TextView mGratefulList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInputGratefulList = (EditText) findViewById(R.id.grateful_input_box);
+
+        mGratefulList = (TextView) findViewById(R.id.grateful_list);
+
     }
+
 }
