aa9fe0511114406907d585015bc11fc74e354274
Claire Lu <CXL3@users.noreply.github.com>
2017-11-02 20:40:44 -0700

    update 2
    Add menu and its item in  menu. xml and MainActivity.java
    
    Add string
    
    Add button
    
    Add method to show EditText to TextView
    
    Button clicked pull the method

--- Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/MainActivity.java
+++ Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/MainActivity.java
@@ -1,9 +1,15 @@
 package com.appfunlu.lawofattraction;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +25,38 @@ public class MainActivity extends AppCompatActivity {
         mInputGratefulList = (EditText) findViewById(R.id.grateful_input_box);
 
         mGratefulList = (TextView) findViewById(R.id.grateful_list);
-
     }
 
+    /**
+     * This Method retrieves the text from EditText and
+     * dislpay it in the TextView
+     */
+    private void showGratefulText(){
+        String textInput = mInputGratefulList.getText().toString();
+        mGratefulList.setText(textInput.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.grateful_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.grateful_tab) {
+            Context context = MainActivity.this;
+            String textToShow = "Grateful list clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void saveGratitudeList(View v) {
+        showGratefulText();
+    }
 }
