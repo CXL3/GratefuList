8464425f021e7b6c4a42ab071c2791f395f64f54
Claire Lu <CXL3@users.noreply.github.com>
2017-11-13 22:04:59 -0800

    Added a direction main page

---
+++ Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/GratitudeActivity.java
@@ -0,0 +1,75 @@
+package com.appfunlu.lawofattraction;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GratitudeActivity extends AppCompatActivity {
+
+    private EditText mInputGratefulList;
+
+    private TextView mGratefulList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gratitude_list);
+
+        mInputGratefulList = (EditText) findViewById(R.id.grateful_input_box);
+
+        mGratefulList = (TextView) findViewById(R.id.grateful_list);
+
+    }
+
+    /**
+     * This Method retrieves the text from EditText and
+     * dislpay it in the TextView
+     */
+    private void showGratefulText(){
+        String gTextInput = mInputGratefulList.getText().toString();
+        mGratefulList.setText(gTextInput.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.grateful_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+
+        if (itemThatWasClickedId == R.id.vision_tab) {
+            Context context = GratitudeActivity.this;
+            Class destinationActivity1 = VisionActivity.class;
+            Intent startVisionActivityIntent = new Intent(context, destinationActivity1);
+            startActivity(startVisionActivityIntent);
+            return true;
+        }
+
+        if (itemThatWasClickedId == R.id.quotes_tab) {
+            Context context = GratitudeActivity.this;
+            Class destinationActivity2 = QuotesActivity.class;
+            Intent startQuotesActivityIntent = new Intent(context, destinationActivity2);
+            startActivity(startQuotesActivityIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void saveGratitudeList(View v) {
+
+        showGratefulText();
+    }
+}
--- Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/MainActivity.java
+++ Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/MainActivity.java
@@ -1,75 +1,64 @@
 package com.appfunlu.lawofattraction;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mInputGratefulList;
-
-    private TextView mGratefulList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
 
-        mInputGratefulList = (EditText) findViewById(R.id.grateful_input_box);
 
-        mGratefulList = (TextView) findViewById(R.id.grateful_list);
+        TextView numbers = (TextView) findViewById(R.id.gratitude_front_page);
 
-    }
+        numbers.setOnClickListener(new View.OnClickListener() {
 
-    /**
-     * This Method retrieves the text from EditText and
-     * dislpay it in the TextView
-     */
-    private void showGratefulText(){
-        String gTextInput = mInputGratefulList.getText().toString();
-        mGratefulList.setText(gTextInput.toString());
-    }
+            @Override
+            public void onClick(View view) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.grateful_menu, menu);
-        return true;
-    }
+                Intent gIntent = new Intent(MainActivity.this, GratitudeActivity.class);
+
+                startActivity(gIntent);
+            }
+        });
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemThatWasClickedId = item.getItemId();
-
-        if (itemThatWasClickedId == R.id.vision_tab) {
-            Context context = MainActivity.this;
-            Class destinationActivity1 = VisionActivity.class;
-            Intent startVisionActivityIntent = new Intent(context, destinationActivity1);
-            startActivity(startVisionActivityIntent);
-            return true;
-        }
-
-        if (itemThatWasClickedId == R.id.quotes_tab) {
-            Context context = MainActivity.this;
-            Class destinationActivity2 = QuotesActivity.class;
-            Intent startQuotesActivityIntent = new Intent(context, destinationActivity2);
-            startActivity(startQuotesActivityIntent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+        TextView family = (TextView) findViewById(R.id.vision_front_page);
 
 
-    public void saveGratitudeList(View v) {
+        family.setOnClickListener(new View.OnClickListener() {
 
-        showGratefulText();
+            @Override
+            public void onClick(View view) {
+
+                Intent vIntent = new Intent(MainActivity.this, VisionActivity.class);
+
+
+                startActivity(vIntent);
+            }
+        });
+
+
+        TextView colors = (TextView) findViewById(R.id.quote_front_page);
+
+        colors.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent qIntent = new Intent(MainActivity.this, QuotesActivity.class);
+
+
+                startActivity(qIntent);
+            }
+        });
+
     }
 }
