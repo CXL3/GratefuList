83a1a590de3b322c8a8eb969966b5f032fb6788c
Claire Lu <CXL3@users.noreply.github.com>
2017-11-22 22:17:25 -0800

    Add Vision pictures activity and layout, also add some desciption for onClick Listener and Intent.

--- Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/MainActivity.java
+++ Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/MainActivity.java
@@ -8,18 +8,28 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * I give variable names to the text views on the main page.
+     */
+
+    private TextView gratitudeFront;
+    private TextView visionFront;
+    private TextView picuturesFront;
+    private TextView quoteFront;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
+
         setContentView(R.layout.activity_main);
 
-
-
-
-
-        TextView gratitudeFront = (TextView) findViewById(R.id.gratitude_front_page);
+        /*
+         * Set the TextView to this variable and set a click listener on it.
+         * the intent will open the GratitudeActivity.
+         */
+        gratitudeFront = (TextView) findViewById(R.id.gratitude_front_page);
         gratitudeFront.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,12 +38,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+         /*
+         * Set the TextView to this variable and set a click listener on it.
+         * the intent will open the VisionActivity
+         */
 
-
-
-
-        TextView VisionFront = (TextView) findViewById(R.id.vision_front_page);
-        VisionFront.setOnClickListener(new View.OnClickListener() {
+        visionFront = (TextView) findViewById(R.id.vision_front_page);
+        visionFront.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -43,12 +54,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        /*
+         * Set the TextView to this variable and set a click listener on it.
+         * the intent will open the PictureActivity
+         */
+
+        picuturesFront = (TextView) findViewById(R.id.picture_front_page);
+        picuturesFront.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent vIntent = new Intent(MainActivity.this, PicturesActivity.class);
+                startActivity(vIntent);
+            }
+        });
 
 
 
 
-        TextView QuoteFront = (TextView) findViewById(R.id.quote_front_page);
-        QuoteFront.setOnClickListener(new View.OnClickListener() {
+        /*
+         * Set the TextView to this variable and set a click listener on it.
+         * the intent will open the QuotesActivity
+         */
+
+
+        quoteFront = (TextView) findViewById(R.id.quote_front_page);
+        quoteFront.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -59,4 +90,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
 
