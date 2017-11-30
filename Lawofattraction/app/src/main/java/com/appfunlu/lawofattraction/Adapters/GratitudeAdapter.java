4c335576f3bab6879b328611aa4fa1eabf2dab06
Claire Lu <CXL3@users.noreply.github.com>
2017-11-30 02:04:36 -0800

    Added ViewHolder for RecyclerView purpose
    1./**
         * Created a class within GratitudeAdapter called GratitudeViewHolder.
         * It extends from the RecyclerView.ViewHolder.
         * This class will hold the gratitude list  in the recycler view
         * Created a TextView called gratefulFinalView.
         * It will show the Grateful List that the users inputted.
         */
    2. /**
             * Created a constructor for GratitudeViewHolde that accepts a view as a parameter.
             * call super on (itemView) and then find gratefulFinalView by ID
             */

--- Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/Adapters/GratitudeAdapter.java
+++ Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/Adapters/GratitudeAdapter.java
@@ -1,5 +1,37 @@
 package com.appfunlu.lawofattraction.Adapters;
 
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.appfunlu.lawofattraction.R;
+
 public class GratitudeAdapter {
+
+
+    /**
+     * Created a class within GratitudeAdapter called GratitudeViewHolder.
+     * It extends from the RecyclerView.ViewHolder.
+     * This class will hold the gratitude list  in the recycler view
+     * Created a TextView called gratefulFinalView.
+     * It will show the Grateful List that the users inputted.
+     */
+    class GratitudeViewHolder extends RecyclerView.ViewHolder {
+        TextView gratefulFinalView;
+
+        /**
+         * Created a constructor for GratitudeViewHolde that accepts a view as a parameter.
+         * call super on (itemView) and then find gratefulFinalView by ID
+         */
+
+        public GratitudeViewHolder(View itemView) {
+            super(itemView);
+            gratefulFinalView = (TextView) itemView.findViewById(R.id.g_recycler_text);
+
+
+
+        }
+    }
+
 }
