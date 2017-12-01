9942af8ca0b00a23b7c5cbb0e9b763a00de65eb8
Claire Lu <CXL3@users.noreply.github.com>
2017-12-01 00:43:01 -0800

    RecyclerViewAdapter
    1. From GratitudeAdapter, extend RecyclerView.Adapter<GratitudeAdapter.GratitudeViewHolder>
    2. Override onCreateViewHolder
    3. Overrde onBindViewHolder
    4.create method that returns the number of items to display.

--- Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/Adapters/GratitudeAdapter.java
+++ Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/Adapters/GratitudeAdapter.java
@@ -1,14 +1,62 @@
 package com.appfunlu.lawofattraction.Adapters;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.appfunlu.lawofattraction.R;
 
-public class GratitudeAdapter {
+/**
+ * From GratitudeAdapter, extend RecyclerView.Adapter<GratitudeAdapter.GratitudeViewHolder>
+ */
 
+public class GratitudeAdapter extends RecyclerView.Adapter<GratitudeAdapter.GratitudeViewHolder> {
+
+    /**
+     * Override the onCreateViewHolder method, since the ViewHolder is created.
+     * @param viewGroup viewGroup The ViewGroup that the ViewHolders are contained within.
+     * @return A new GratitudeViewHolder that holds the View for each list item
+     */
+    @Override
+    public GratitudeViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+        Context gContext = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(gContext);
+
+        boolean shouldAttachToParentImmediately = false;
+        View view = inflater.inflate(R.layout.g_recycler_list_item, viewGroup, shouldAttachToParentImmediately);
+
+        return new GratitudeViewHolder(view);
+    }
+
+
+
+    /**
+     * Override onBindViewHolder.
+     * It is is called by the RecyclerView to display the data at the specified position.
+     * @param gViewholder The ViewHolder which should be updated to represent the contents of the
+    item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(GratitudeViewHolder gViewholder, int position) {
+
+    }
+
+
+    /**
+     * This method simply returns the number of items to display.
+     * @return 0 for now.
+     */
+    @Override
+    public int getItemCount() {
+        return 0;
+
+    }
 
     /**
      * Created a class within GratitudeAdapter called GratitudeViewHolder.
