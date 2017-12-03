dbf4743d30d7d22e83924540ed681ff1a1fe20c3
Claire Lu <CXL3@users.noreply.github.com>
2017-12-03 14:00:17 -0800

    Create a contract
    1.created GratitudeContract
    2.I created the inner class that defines that table contents of the Gratitude Table.
    3.I have created static final members for the table name and the db column.

---
+++ Lawofattraction/app/src/main/java/com/appfunlu/lawofattraction/Data/GratitudeContract.java
@@ -0,0 +1,23 @@
+package com.appfunlu.lawofattraction.Data;
+
+import android.provider.BaseColumns;
+
+/*
+ * This defines the table and column names for the gratitude database.
+ */
+public class GratitudeContract {
+
+    /**
+     * I created the inner class that defines that table contents of the Gratitude Table.
+     * This class that implements the BaseColumns interface.
+     */
+    public static final class GratitudeEntry implements BaseColumns {
+
+        /* I have created static final members for the table name and the db column.*/
+        public static final String TABLE_NAME = "Gratitude";
+        public static final String COLUMN_GRATEFUL_LIST = "gratefulList";
+
+
+    }
+
+}
